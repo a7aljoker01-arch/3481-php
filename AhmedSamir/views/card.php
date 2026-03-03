95c12148607da9dfab9b4dc647c73baed58c4e32
Ahmed Samir <ahmedsamir7685@gmail.com>
2026-03-03 20:53:29 +0200

    mes

--- AhmedSamir/views/card.php
+++ AhmedSamir/views/card.php
@@ -1,35 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Card</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="flex items-center justify-center p-6 flex-wrap">
 
+<body class="flex items-center justify-center p-6 flex-wrap gap-5">
 
-        <div class="">
-            <img
-                src="<?= $product['thumbnail']; ?>"
-                alt="<?= $product['title']; ?>"
-                class="w-full h-56 object-cover transform group-hover:scale-110 transition duration-500"
-            >
-            <span class="absolute top-4 left-4 bg-black/80 text-white text-sm px-3 py-1 rounded-full backdrop-blur">
-                $<?= $product['price']; ?>
-            </span>
-        </div>
+    <div class="border border-gray-200 lg:w-3/12 md:w-4/12 w-full mb-10 p-5 rounded-2xl">
+
+        <span class="bg-black/80 text-white text-sm px-3 py-1 rounded-full">
+            $<?= $product['price']; ?>
+        </span>
+        <img
+            src="<?= $product['thumbnail']; ?>"
+            alt="<?= $product['title']; ?>"
+            class="w-full h-56 object-cover transform group-hover:scale-110 transition duration-500">
 
         <div class="p-6">
-            <h3 class="text-xl font-semibold text-gray-800 group-hover:text-black transition">
+            <h3 class="text-xl font-semibold text-gray-800 ">
                 <?= $product['title']; ?>
             </h3>
-
-            <button class="mt-6 w-full bg-black text-white py-3 rounded-xl hover:bg-gray-800 active:scale-95 transition">
-                View Product
-            </button>
         </div>
+    </div>
 
 
 </body>
+
 </html>
