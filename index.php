a4d67c51e343426d74899c7b36d950d0c3b9d47e
Ahmad Hassan <a7aljoker01@gmail.com>
2026-03-05 02:03:52 +0200

    PHP varibles

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Ahmad";
+$food = "pizza";
+echo " {$name} like {$food} .";
+/* */
+$age = 21;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
