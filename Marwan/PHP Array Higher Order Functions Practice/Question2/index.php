774a2aadfe3d505545083e5f8c06af5102cb9700
yaraabdalla-coder <yaraabdalla295@gmail.com>
2026-03-07 14:42:52 +0200

    update files

--- Marwan/PHP Array Higher Order Functions Practice/Question2/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$numbers = [10, 15, 20, 25, 30];
-
-$result = array_map(function ($number) {
-    return $number * $number;
-}, $numbers);
-var_dump($result);
--- Marwan/challenge add five files in forked file/file4.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "file4 commit";
