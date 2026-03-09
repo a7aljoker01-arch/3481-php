d745d615e702c9ded1c5e39db4a4ebab785dca43
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-03-09 11:00:37 +0200

    Largest of Three condition

--- App/ALY/Challenge_17/Conditions.php
+++ App/ALY/Challenge_17/Conditions.php
@@ -11,6 +11,25 @@ class Conditions
         else{
             echo "This number is odd";
         }
-        
+    }
+
+    public static function largest($a, $b, $c)
+    {
+        if ($a > $b) {
+            if ($a > $c){
+                echo "$a is the largest";
+
+            } else{
+                echo "$c is the largest";
+            }
+
+        } else {
+            if ($b > $c){
+                echo "$b is the largest";
+
+            } else{
+                echo "$c is the largest";
+            }
+        }
     }
 }
--- aly/cond.php
+++ aly/cond.php
@@ -4,3 +4,5 @@ require_once '../vendor/autoload.php';
 use App\Aly\Challenge_17\Conditions;
 
 Conditions::if(num: 4);
+
+Conditions::largest(a:45, b:35, c:50);
